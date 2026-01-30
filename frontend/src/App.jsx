8c1c081f999b38a391f0cad2e27b47b8cee1a897
SURAJ ARS <70328836+SURAJARS@users.noreply.github.com>
2026-01-30 21:24:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header, Navigation, Footer } from './components/Layout';
 import Home from './pages/Home';
 import Snacks from './pages/Snacks';
@@ -40,3 +40,4 @@ function App() {
 }
 
 export default App;
+
